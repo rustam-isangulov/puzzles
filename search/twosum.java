d524a55fbd83274bf481a73cfc36eecaa0653b53
rustam-isangulov <rustam.pro@icloud.com>
2022-06-24 10:18:14 +0100

    Add the problem description.

--- search/twosum.java
+++ search/twosum.java
@@ -2,6 +2,16 @@ package search;
 
 import java.util.*;
 
+/*
+tag: search
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
 public class twosum {
     public static void main(String... args) {
 	System.out.println("twosum is starting... ");
