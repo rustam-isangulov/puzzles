7a6c56e785f869c84c2f23b1d9247e4ee5810d1c
Rustam.Isangulov <Rustam.Isangulov@horizondiscovery.com>
2023-11-13 19:31:50 +0000

    Added a more resonable LINQ based solution for the cli arg list

--- csharp/solutions/interviews/Av.cs
+++ csharp/solutions/interviews/Av.cs
@@ -1,124 +1,191 @@
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 
 namespace solutions;
 
 public class AvSolution
 {
-    public int solution(string[] args)
+    public int SolutionLINQ(string[] args)
     {
         const int INVALID = -1;
         const int OK = 0;
         const int HELP = 1;
 
-        try
+        var noValueTokens = new HashSet<string>() { "--HELP" };
+
+
+        var allParams = args?.Select((s, i) => (name: s.ToUpper(), pos: i))
+                             .Where(it => it.name.StartsWith("--")) ?? Enumerable.Empty<(string, int)>();
+
+
+        var paramsRightPos = allParams.Skip(1)
+                                      .Select(it => it.pos)
+                                      .Concat(new int[] { args?.Length ?? 0 });
+
+        var paramValueSlices = allParams.Zip(paramsRightPos, (param, rightPos) => (name: param.name, left: param.pos, right: rightPos))
+                                        .Select(it => (paramName: it.name, values: args?[(it.left + 1)..it.right] ?? Enumerable.Empty<string>()));
+
+        // FORMAT CHECKS
+
+        var noValueParamsWithValues = paramValueSlices.Where(it => noValueTokens.Contains(it.paramName))
+                                                      .Where(it => it.values.Any());
+
+        var repeatedParams = paramValueSlices.GroupBy(it => it.paramName, it => it, (key, values) => (name: key, count: values.Count()))
+                                             .Where(it => it.count > 1);
+
+        var valueParamsWithNoValues = paramValueSlices.Where(it => !noValueTokens.Contains(it.paramName))
+                                                       .Where(it => !it.values.Any());
+
+        var paramsWithMultipleValues = paramValueSlices.Where(it => !noValueTokens.Contains(it.paramName))
+                                                       .Where(it => it.values.Count() > 1);
+        
+        bool startsWithParam = allParams.Where( it => it.pos == 0 ).Any();
+        
+        if (!paramValueSlices.Any() 
+                || noValueParamsWithValues.Any() 
+                || repeatedParams.Any() 
+                || paramsWithMultipleValues.Any() 
+                || valueParamsWithNoValues.Any()
+                || !startsWithParam)
         {
-            if (args == null || args.Length == 0)
+            return INVALID;
+        }
+
+
+        // VALUE CHECKS
+
+        var rules = new Dictionary<string, Func<string, bool>>()
+        {
+            {"--NAME", s => s.Length >=3 && s.Length <= 10 },
+            {"--COUNT", s => int.TryParse(s, out int value) && value < 20 }
+        };
+
+        var paramsWithBadValues = paramValueSlices.Where(it => !noValueTokens.Contains(it.paramName))
+                                                  .Where(it => !rules.GetValueOrDefault(it.paramName, s => true)(it.values.First()));
+
+        if (paramsWithBadValues.Any())
+        {
+            return INVALID;
+        }
+
+        // OK vs HELP
+
+        if (allParams.Where(it => it.name == "--HELP").Any())
+        {
+            return HELP;
+        }
+
+        return OK;
+    }
+
+    public int Solution(string[] args)
+    {
+        const int INVALID = -1;
+        const int OK = 0;
+        const int HELP = 1;
+
+        if (args == null || args.Length == 0)
+        {
+            return INVALID;
+        }
+
+        //
+        // process to get token - value pairs
+        //
+
+        int pos = 0;
+        string currentToken = "";
+        var argsDict = new Dictionary<string, string>();
+
+        while (pos < args.Length)
+        {
+            if (args[pos].ToUpper() == "--NAME")
             {
-                return INVALID;
+                currentToken = args[pos].ToUpper();
             }
-
-            //
-            // process to get token - value pairs
-            //
-
-            int pos = 0;
-            string currentToken = "";
-            var argsDict = new Dictionary<string, string>();
-
-            while (pos < args.Length)
+            else if (args[pos].ToUpper() == "--COUNT")
             {
-                if (args[pos].ToUpper() == "--NAME")
-                {
-                    currentToken = args[pos].ToUpper();
-                }
-                else if (args[pos].ToUpper() == "--COUNT")
-                {
-                    currentToken = args[pos].ToUpper() ;
-                }
-                else if (args[pos].ToUpper() == "--HELP")
-                {
-                    currentToken = args[pos].ToUpper();
+                currentToken = args[pos].ToUpper();
+            }
+            else if (args[pos].ToUpper() == "--HELP")
+            {
+                currentToken = args[pos].ToUpper();
 
+                if (argsDict.ContainsKey(currentToken))
+                {
+                    // second appearance of the same key
+                    return INVALID;
+                }
+
+                argsDict[currentToken] = "YES";
+            }
+            else
+            {
+                if (currentToken != "")
+                {
                     if (argsDict.ContainsKey(currentToken))
                     {
                         // second appearance of the same key
                         return INVALID;
                     }
 
-                    argsDict[currentToken] = "YES";
-                }
-                else 
-                {
-                    if (currentToken != "")
-                    {
-                        if (argsDict.ContainsKey(currentToken))
-                        {
-                            // second appearance of the same key
-                            return INVALID;
-                        }
-
-                        argsDict[currentToken] = args[pos];
-                    }
-                    else
-                    {
-                        // value with no key
-                        return INVALID;
-                    }
-
-                    currentToken = "";
-                }
-
-                pos++;
-            }
-
-            //
-            // validate
-            //
-
-            // EMTRY token list
-            if (argsDict.Count == 0)
-            {
-                return INVALID;
-            }
-
-            // COUNT
-            if (argsDict.ContainsKey("--COUNT"))
-            {
-                if (Int32.TryParse(argsDict["--COUNT"], out int j))
-                {
-                    if (j < 10 || j > 100)
-                    {
-                        return INVALID;
-                    }
+                    argsDict[currentToken] = args[pos];
                 }
                 else
                 {
+                    // value with no key
                     return INVALID;
                 }
+
+                currentToken = "";
             }
 
-            // NAME
-            if (argsDict.ContainsKey("--NAME"))
+            pos++;
+        }
+
+        //
+        // validate
+        //
+
+        // EMPTY token list
+        if (argsDict.Count == 0)
+        {
+            return INVALID;
+        }
+
+        // COUNT
+        if (argsDict.ContainsKey("--COUNT"))
+        {
+            if (Int32.TryParse(argsDict["--COUNT"], out int j))
             {
-                if (argsDict["--NAME"].Length < 3 || argsDict["--NAME"].Length > 10)
+                if (j < 10 || j > 100)
                 {
                     return INVALID;
                 }
             }
-
-            // react on HELP
-            if ( argsDict.ContainsKey("--HELP"))
+            else
             {
-                return HELP;
+                return INVALID;
             }
-
-            return OK;
         }
-        finally
-        {}
 
-        return INVALID;
+        // NAME
+        if (argsDict.ContainsKey("--NAME"))
+        {
+            if (argsDict["--NAME"].Length < 3 || argsDict["--NAME"].Length > 10)
+            {
+                return INVALID;
+            }
+        }
+
+        // react on HELP
+        if (argsDict.ContainsKey("--HELP"))
+        {
+            return HELP;
+        }
+
+        return OK;
     }
 }
--- csharp/tests/interviews/AvTest.cs
+++ csharp/tests/interviews/AvTest.cs
@@ -5,11 +5,21 @@ namespace tests;
 public class AvSolutionTest
 {
     [Theory, MemberData(nameof(TestData))]
+    public void TestMultiLINQ(string[] inputArgs, int expectedValue)
+    {
+        var instance = new AvSolution();
+
+        var resultValue = instance.SolutionLINQ(inputArgs);
+
+        Assert.Equal(expectedValue, resultValue);
+    }
+
+    [Theory, MemberData(nameof(TestData))]
     public void TestMulti(string[] inputArgs, int expectedValue)
     {
         var instance = new AvSolution();
 
-        var resultValue = instance.solution(inputArgs);
+        var resultValue = instance.Solution(inputArgs);
 
         Assert.Equal(expectedValue, resultValue);
     }
@@ -22,55 +32,60 @@ public class AvSolutionTest
             null,
             -1
         },        
-        // new object[]
-        // {
-        //     new string[0],
-        //     -1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--name", "SOME_NAME", "--count", "10", "--count", "10"},
-        //     -1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--name", "SOME_NAME", "--help", "--count", "10"},
-        //     1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--name", "SOME_NAME", "--count", "10"},
-        //     0
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--name", "SOME_NAME", "--count", "101"},
-        //     -1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--name", "SOME_NAME", "--count", "101", "something"},
-        //     -1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--count", "meh"},
-        //     -1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--help", "meh"},
-        //     -1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--help"},
-        //     1
-        // },
-        // new object[]
-        // {
-        //     new string[]{"--name"},
-        //     -1
-        // },
+        new object[]
+        {
+            new string[0],
+            -1
+        },
+        new object[]
+        {
+            new string[]{"--name", "SOME_NAME", "--count", "10", "--count", "10"},
+            -1
+        },
+        new object[]
+        {
+            new string[]{"--name", "SOME_NAME", "--help", "--count", "10"},
+            1
+        },
+        new object[]
+        {
+            new string[]{"random string at the start", "--name", "SOME_NAME", "--help", "--count", "10"},
+            -1
+        },
+        new object[]
+        {
+            new string[]{"--name", "SOME_NAME", "--count", "10"},
+            0
+        },
+        new object[]
+        {
+            new string[]{"--name", "SOME_NAME", "--count", "101"},
+            -1
+        },
+        new object[]
+        {
+            new string[]{"--name", "SOME_NAME", "--count", "101", "something"},
+            -1
+        },
+        new object[]
+        {
+            new string[]{"--count", "meh"},
+            -1
+        },
+        new object[]
+        {
+            new string[]{"--help", "meh"},
+            -1
+        },
+        new object[]
+        {
+            new string[]{"--help"},
+            1
+        },
+        new object[]
+        {
+            new string[]{"--name"},
+            -1
+        },
     };
 }
